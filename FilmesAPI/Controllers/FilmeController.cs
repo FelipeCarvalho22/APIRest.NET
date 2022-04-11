7ac585218df25eb8cc4df159e2f2307439ffdcf3
Felipe Silva <felipe-silva22@hotmail.com>
2022-04-11 10:24:49 -0300

    Primeira Commit API Rest

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,41 @@
+﻿using FilmesAPI.Controllers.Models;
+using Microsoft.AspNetCore.Mvc;                                             //Biblioteca dotNET responsavel pelo CORE HTTP
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]                                                         //Anotação que define a API Controller
+    [Route("[controller]")]                                                 //Anotação que define a Rota da API
+    public class FilmeController : ControllerBase                           //Deve herdar do ControllerBase
+    {
+        private static int id = 1;
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]                                                                              //Verbo HTTP que indica que a ação seguinte é um POST
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperarFilmePorId), new { Id = filme.Id }, filme);  //Segundo as melhores praticas do REST esse retorno nos fornece a rota para acesso
+                                                                                                //deste objeto criado
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarFilmes()                                                  //Por ser uma lista ele implementa a intermace IEnumerable,
+                                                                                                //sendo um melhor tipo para esta aplicação
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]                                                                       //Ao especificarmos o parametro, definimos que
+                                                                                                //quando passado na URL este GET sera acionado
+        public IActionResult RecuperarFilmePorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);                       //Sintaxe JS usando uma expressão Lambda
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ FilmesAPI/Controllers/Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Controllers.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage ="O Campo Titulo é Obrigatório")]                                //Esta anotação torna obrigatórioa a escrita de algo neste campo
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage ="O Campo Diretor é Obrigatório")]
+        public string Diretor { get; set; }
+
+        [StringLength(30, ErrorMessage = "Genero não pode ter mais de 30 caracteres")]          //Esta anotação limita a quantidade de caracteres no campo
+        public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage ="Limites devem ser entre 1-600min")]                       //Esta anotação limita o range do campo
+        public int Duracao { get; set; }
+        public int Id { get; set; }
+
+        //ErrorMesage define o que será aprensentado ao usuario caso um erro ocorra, bom para tradução das mensagens
+    }
+}
